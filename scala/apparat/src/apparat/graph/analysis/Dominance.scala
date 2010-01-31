879dbd119393cce9e838d640290566cc8ffe8329
joaebert <devnull@localhost>
2010-01-31 15:34:43 +0000

    - dominance analysis

--- scala/apparat/src/apparat/graph/analysis/Dominance.scala
+++ scala/apparat/src/apparat/graph/analysis/Dominance.scala
@@ -20,33 +20,141 @@
  */
 package apparat.graph.analysis
 
-import java.util.ArrayList
 import apparat.graph.{ControlFlow, GraphLike}
 
 /**
  * @author Joa Ebert
  */
 class Dominance[V](val graph: GraphLike[V]) {
-	private lazy val postorder = {
-		def findEntry(): V = graph match {
+	lazy val entry: V = {
+		def search(): V = graph match {
 			case controlFlow: ControlFlow[_] => controlFlow.entryVertex.asInstanceOf[V]//ugly :(
 			case _ => {
 				for(vertex <- graph.verticesIterator if (graph indegreeOf vertex) == 0) {
 					return vertex
 				}
-				
+
 				error("No entry vertex found.")
 			}
 		}
+		
+		search()
+	}
 
-		val entry = findEntry
+	private lazy val postorder = graph dft entry toList
+
+	private lazy val reversePostorder = postorder.reverse
+
+	private lazy val doms = {
+		//
+		// "A Simple, Fast Dominance Algorithm"
+		//
+		// Keith D. Cooper et al.
+		// Rice University, Houston, TX
+		//
+		// http://www.cs.rice.edu/~keith/EMBED/dom.pdf
+		// Page 13
+		//
+
+		var result = Map(entry -> entry)
+		val rp = reversePostorder filterNot (_ == entry)
+
+		def pick(map: Map[V, V], iterable: Iterable[V]): V = {
+			for(vertex <- iterable) {
+				if(map contains vertex) {
+					return vertex
+				}
+			}
+			
+			error("Unreachable...")
+		}
+
+		def intersect(b1: V, b2: V) = {
+			var finger1 = b1
+			var finger2 = b2
+
+			while(finger1 != finger2) {
+				while(postorder.indexOf(finger1) < postorder.indexOf(finger2))
+					finger1 = result(finger1)
+
+				while(postorder.indexOf(finger2) < postorder.indexOf(finger1))
+					finger2 = result(finger2)
+			}
+
+			finger1
+		}
+
+		def loop(): Unit = {
+			var changed = false
+
+			for(b <- rp) {
+				val predecessorsTmp = graph predecessorsOf b
+				var newIDom = pick(result, predecessorsTmp)
+				val predecessors = predecessorsTmp filterNot (_ == newIDom)
+
+				for(p <- predecessors) {
+					result get p match {
+						case Some(vertex) => {
+							newIDom = intersect(vertex, newIDom)
+						}
+						case None =>
+					}
+
+					result get b match {
+						case Some(old) => {
+							if(old != newIDom) {
+								result = result updated (b, newIDom)
+								changed = true
+							}
+						}
+						case None => {
+							result = result + (b -> newIDom)
+							changed = true
+						}
+					}
+				}
+			}
+
+			if(changed) {
+				loop()
+			}
+		}
+
+		loop()
+		
+		result
 	}
 
 	private lazy val frontiers = {
-		Map.empty[V, List[V]]
+		//
+		// "A Simple, Fast Dominance Algorithm"
+		//
+		// Keith D. Cooper et al.
+		// Rice University, Houston, TX
+		//
+		// http://www.cs.rice.edu/~keith/EMBED/dom.pdf
+		// Page 18
+		//
+
+		var result = graph vertexMap (v => List.empty[V])
+
+		for(b <- graph.verticesIterator) {
+			val predecessors = graph predecessorsOf b
+
+			if(predecessors.size > 1) {
+				for(p <- predecessors) {
+					var runner = p
+
+					while(runner != doms(b)) {
+						result = result updated (runner, b :: result(runner))
+						runner = doms(runner)
+					}
+				}
+			}
+		}
+
+		result
 	}
 
-	private lazy val doms = {
-		Map.empty[V,V]
-	}
+	def frontiersOf(vertex: V) = frontiers get vertex
 }
