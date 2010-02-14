afc5dc148c51b82dd5e3ed661f861b827e3d32e0
leclech.patrick <devnull@localhost>
2010-02-14 15:10:23 +0000

    - first sketch for ant tasks

---
+++ scala/apparat/src/apparat/embedding/ant/ApparatTask.scala
@@ -0,0 +1,57 @@
+package apparat.embedding.ant
+
+/*
+ * This file is part of Apparat.
+ * 
+ * Apparat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Apparat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Apparat. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright (C) 2009 Joa Ebert
+ * http://www.joa-ebert.com/
+ * 
+ * User: Patrick Le Clec'h
+ * Date: 12 févr. 2010
+ * Time: 12:50:50
+ */
+import org.apache.tools.ant.taskdefs.Java
+import collection.mutable.HashMap
+
+class ApparatTask extends Java {
+	val arguments: HashMap[String, String] = HashMap.empty
+
+//	@deprecated("Use setArgument instead of createArg")
+	override def createArg = error("Use setArgument instead of createArg")
+
+	def setArgument(name: String, value: Any) = {
+		arguments(name) = value.toString
+	}
+
+	override def execute = {
+		clearArgs()
+
+		arguments.keysIterator.foreach(name => {
+			super.createArg().setValue("-" + name)
+			super.createArg().setValue(arguments(name))
+		})
+
+		super.execute()
+	}
+
+	def out = arguments.getOrElse("o", "")
+
+	def int = arguments.getOrElse("i", "")
+
+	def setIn(name: String) = setArgument("i", name)
+
+	def setOut(name: String) = setArgument("o", name)
+}
---
+++ scala/apparat/src/apparat/embedding/ant/stripper/StripperTask.scala
@@ -0,0 +1,33 @@
+package apparat.embedding.ant.stripper
+
+import apparat.tools.stripper.Stripper
+import apparat.embedding.ant.ApparatTask
+
+/*
+ * This file is part of Apparat.
+ * 
+ * Apparat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Apparat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Apparat. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright (C) 2009 Joa Ebert
+ * http://www.joa-ebert.com/
+ * 
+ * User: Patrick Le Clec'h
+ * Date: 12 févr. 2010
+ * Time: 13:45:35
+ */
+
+class StripperTask extends ApparatTask {
+	setTaskName("stripper")
+	setClassname(Stripper.getClass.getName dropRight 1)
+}
