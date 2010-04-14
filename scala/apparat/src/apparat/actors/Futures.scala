67286feef4b3226f47bb1c126cdbb8f2588c1962
joaebert <devnull@localhost>
2010-04-14 11:57:25 +0000

    - mono thread mode now works with just 1 thread

--- scala/apparat/src/apparat/actors/Futures.scala
+++ scala/apparat/src/apparat/actors/Futures.scala
@@ -1,16 +1,16 @@
 package apparat.actors
 
-import scala.actors.{Future => SFuture, Futures => SFutures}
+import scala.actors.{Futures => SFutures}
 
 object Futures {
 	private val enabled = Actor.threadsEnabled
 
-	def future[T](body: => T): SFuture[T] = {
+	def future[T](body: => T): () => T = {
 		if(enabled) {
 			SFutures.future(body)
 		} else {
 			val result: T = body
-			SFutures.future { result }
+			() => { result }
 		}
 	}
 }
