3a19fbb6f506a55d70b667bb573ffdc316f847c3
Joa Ebert <devnull@localhost>
2010-05-06 14:02:18 +0200

    Added AbcDependencyGraphBuilder

--- scala/apparat/src/apparat/graph/immutable/BytecodeControlFlowGraphBuilder.scala
+++ scala/apparat/src/apparat/graph/immutable/BytecodeControlFlowGraphBuilder.scala
@@ -1,3 +1,27 @@
+/*
+ * This file is part of Apparat.
+ *
+ * Apparat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Apparat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Apparat. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2009 Joa Ebert
+ * http://www.joa-ebert.com/
+ *
+ * User: Patrick Le Clec'h
+ * Date: 31 janv. 2010
+ * Time: 19:52:30
+ */
+
 package apparat.graph.immutable
 
 import apparat.bytecode.{Marker, Bytecode}
@@ -5,31 +29,7 @@ import apparat.bytecode.operations._
 import apparat.graph._
 import annotation.tailrec
 
-/*
- * This file is part of Apparat.
- * 
- * Apparat is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * Apparat is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with Apparat. If not, see <http://www.gnu.org/licenses/>.
- * 
- * Copyright (C) 2009 Joa Ebert
- * http://www.joa-ebert.com/
- * 
- * User: Patrick Le Clec'h
- * Date: 31 janv. 2010
- * Time: 19:52:30
- */
-
-object BytecodeControlFlowGraphBuilder {
+object BytecodeControlFlowGraphBuilder extends (Bytecode => BytecodeControlFlowGraph[ImmutableAbstractOpBlockVertex]) {
 	def apply(bytecode: Bytecode) = {
 		import collection.mutable.{Queue}
 
