c20092e163d29cee71123686e1ed36bb7eadd5ce
Joa Ebert <devnull@localhost>
2010-05-07 16:16:47 +0200

    fixed deflate for big files

--- scala/apparat/src/apparat/utils/Deflate.scala
+++ scala/apparat/src/apparat/utils/Deflate.scala
@@ -23,6 +23,8 @@ package apparat.utils
 import java.io.{
 	OutputStream => JOutputStream,
 	File => JFile,
+	FileOutputStream => JFileOutputStream,
+	FileInputStream => JFileInputStream,
 	IOException => JIOException,
 	BufferedReader => JBufferedReader,
 	ByteArrayOutputStream => JByteArrayOutputStream,
@@ -30,12 +32,18 @@ import java.io.{
 import java.util.zip.{Deflater => JDeflater, Adler32 => JAdler32}
 import java.lang.{ProcessBuilder => JProcessBuilder}
 import apparat.tools.ApparatLog
+import apparat.utils.IO._
 
 /**
  * @author Joa Ebert
  */
 object Deflate {
-	private var _7z = System.getProperty("apparat.7z", "true").toLowerCase == "true"
+	private var _7z = System.getProperty("apparat.7z.enabled", "true").toLowerCase == "true"
+	private val _7zexe = System.getProperty("apparat.7z.path", "7z" + (System getProperty "os.name" indexOf "Windows" match {
+		case -1 => ""
+		case _ => ".exe"
+	}))
+
 	private lazy val adler32 = new JAdler32()
 
 	def compress(bytes: Array[Byte], output: JOutputStream) = {
@@ -64,91 +72,11 @@ object Deflate {
 
 	private def compressUsing7z(bytes: Array[Byte], output: JOutputStream) = {
 		try {
-			val builder = new JProcessBuilder("7z", "a", "apparat", "-tgzip", "-mx9", "-siswf", "-so")
-			val process = builder.start()
-			val outputStream = process.getOutputStream
-			val inputStream = process.getInputStream
-
-			outputStream write bytes
-			outputStream.close()
-
-			val buffer = new Array[Byte](0x8000)
-			var bytesTotal = 0
-			val byteArrayOutputStream = new JByteArrayOutputStream()
-
-			while(inputStream.available != 0) {
-				val bytesRead = inputStream.read(buffer)
-				bytesTotal += bytesRead
-				byteArrayOutputStream.write(buffer, 0, bytesRead)
-				Thread sleep 8// Can we fix this?
+			if(bytes.length < 0x40000) {
+				compress7zInSTDIO(bytes, output)
+			} else {
+				compress7zOnDisk(bytes, output)
 			}
-
-			inputStream.close()
-			
-			assert(0 == process.waitFor())
-			
-			// GZIP specification:
-			// http://www.ietf.org/rfc/rfc1952
-
-			val gzipBuffer = byteArrayOutputStream.toByteArray
-
-			assert((gzipBuffer(0) & 0xff) == 0x1f, "GZip header is corrupt.")
-			assert((gzipBuffer(1) & 0xff) == 0x8b, "GZip header is corrupt.")
-			assert((gzipBuffer(2) & 0xff) == 0x08, "Deflate stream required.")
-
-			val flags = gzipBuffer(3) & 0xff
-
-			assert(0 == (flags & (1 << 0)), "FTEXT must not be set.")
-			
-			// Skip 4b of modification time
-			// XFL is unimportant (byte 8)
-			// OS is unimportant (byte 9)
-			
-			var bufferPos = 10
-
-			if(0 != (flags & (1 << 2))) {
-				bufferPos += (gzipBuffer(10) & 0xff)
-			}
-
-			if(0 != (flags & (1 << 3))) {
-				while(gzipBuffer(bufferPos) != 0x00) {
-					bufferPos += 1
-				}
-
-				bufferPos += 1
-			}
-
-			if(0!= (flags & (1 << 4))) {
-				while(gzipBuffer(bufferPos) != 0x00) {
-					bufferPos += 1
-				}
-
-				bufferPos += 1
-			}
-
-			if(0 != (flags & (1 << 1))) {
-				bufferPos += 2
-			}
-
-			adler32.reset()
-			adler32 update bytes
-
-			val checksum = adler32.getValue().asInstanceOf[Int]
-
-			//ZLIB Header
-			output write 0x78
-			output write 0xda
-
-			// Deflate Stream
-			output.write(gzipBuffer, bufferPos, gzipBuffer.length - bufferPos - 8)
-
-			// Adler32 Checksum
-			output write ((checksum >> 0x18) & 0xff)
-			output write ((checksum >> 0x10) & 0xff)
-			output write ((checksum >> 0x08) & 0xff)
-			output write (checksum & 0xff)
-
-			output.flush()
 		} catch {
 			case ioException: JIOException => {
 				_7z = false
@@ -163,5 +91,124 @@ object Deflate {
 				compressUsingDeflater(bytes, output)
 			}
 		}
+	}
+
+	private def compress7zOnDisk(bytes: Array[Byte], output: JOutputStream) = {
+		val gzInput = JFile.createTempFile("apparat", "input")
+		val gzOutput = JFile.createTempFile("apparat", "output")
+
+		gzInput.deleteOnExit
+		gzOutput.deleteOnExit
+
+		using(new JFileOutputStream(gzInput)) { _ write bytes }
+
+		val builder = new JProcessBuilder(_7zexe, "a", gzOutput.getAbsolutePath, "-tgzip", "-mx9", gzInput.getAbsolutePath)
+		val process = builder.start()
+
+		ApparatLog("Waiting for 7z ...")
+		assert(0 == process.waitFor())
+
+		val sevenZipOutput = new JFile(gzOutput.getAbsolutePath + ".gz")
+		val fileInputStream = new JFileInputStream(sevenZipOutput)
+
+		sevenZipOutput.deleteOnExit()
+
+		try {
+			writeGZIP(bytes, output, byteArrayOf(fileInputStream))
+		} finally {
+			try { fileInputStream.close() } catch { case _ => {} }
+			try { gzInput.delete() } catch { case _ => {} }
+			try { gzOutput.delete() } catch { case _ => {} }
+			try { sevenZipOutput.delete() } catch { case _ => {} }
+		}
+	}
+
+	private def compress7zInSTDIO(bytes: Array[Byte], output: JOutputStream) = {
+		val builder = new JProcessBuilder(_7zexe, "a", "apparat", "-tgzip", "-mx9", "-siswf", "-so")
+		val process = builder.start()
+		val outputStream = process.getOutputStream
+		val inputStream = process.getInputStream
+
+		outputStream write bytes
+		outputStream.close()
+
+		val buffer = new Array[Byte](0x8000)
+		var bytesTotal = 0
+		val byteArrayOutputStream = new JByteArrayOutputStream()
+
+		ApparatLog("Waiting for 7z ...")
+		
+		while(inputStream.available != 0) {
+			val bytesRead = inputStream.read(buffer)
+			bytesTotal += bytesRead
+			byteArrayOutputStream.write(buffer, 0, bytesRead)
+			Thread sleep 8// Can we fix this?
+		}
+
+		inputStream.close()
+		assert(0 == process.waitFor())
+		writeGZIP(bytes, output, byteArrayOutputStream.toByteArray)
+	}
+
+	private def writeGZIP(bytes: Array[Byte], output: JOutputStream, gzipBuffer: Array[Byte]) = {
+		// GZIP specification:
+		// http://www.ietf.org/rfc/rfc1952
+		assert((gzipBuffer(0) & 0xff) == 0x1f, "GZip header is corrupt.")
+		assert((gzipBuffer(1) & 0xff) == 0x8b, "GZip header is corrupt.")
+		assert((gzipBuffer(2) & 0xff) == 0x08, "Deflate stream required.")
+
+		val flags = gzipBuffer(3) & 0xff
+
+		assert(0 == (flags & (1 << 0)), "FTEXT must not be set.")
+
+		// Skip 4b of modification time
+		// XFL is unimportant (byte 8)
+		// OS is unimportant (byte 9)
+
+		var bufferPos = 10
+
+		if(0 != (flags & (1 << 2))) {
+			bufferPos += (gzipBuffer(10) & 0xff)
+		}
+
+		if(0 != (flags & (1 << 3))) {
+			while(gzipBuffer(bufferPos) != 0x00) {
+				bufferPos += 1
+			}
+
+			bufferPos += 1
+		}
+
+		if(0!= (flags & (1 << 4))) {
+			while(gzipBuffer(bufferPos) != 0x00) {
+				bufferPos += 1
+			}
+
+			bufferPos += 1
+		}
+
+		if(0 != (flags & (1 << 1))) {
+			bufferPos += 2
+		}
+
+		adler32.reset()
+		adler32 update bytes
+
+		val checksum = adler32.getValue().asInstanceOf[Int]
+
+		//ZLIB Header
+		output write 0x78
+		output write 0xda
+
+		// Deflate Stream
+		output.write(gzipBuffer, bufferPos, gzipBuffer.length - bufferPos - 8)
+
+		// Adler32 Checksum
+		output write ((checksum >> 0x18) & 0xff)
+		output write ((checksum >> 0x10) & 0xff)
+		output write ((checksum >> 0x08) & 0xff)
+		output write (checksum & 0xff)
+
+		output.flush()
 	}
 }
