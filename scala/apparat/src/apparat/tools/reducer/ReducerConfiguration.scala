23e975ddc07c02c4741adcce9745db1010a23a27
Joa Ebert <devnull@localhost>
2010-06-07 10:20:22 +0200

    Added typed Reducer configuration.

---
+++ scala/apparat/src/apparat/tools/reducer/ReducerConfiguration.scala
@@ -0,0 +1,48 @@
+/*
+ * This file is part of Apparat.
+ *
+ * Apparat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Apparat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Apparat. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2009 Joa Ebert
+ * http://www.joa-ebert.com/
+ *
+ */
+package apparat.tools.reducer
+
+import java.io.{File => JFile}
+
+/**
+ * @author Joa Ebert
+ */
+trait ReducerConfiguration {
+	/**
+	 * The input file.
+	 */
+	def input: JFile
+
+	/**
+	 * The output file.
+	 */
+	def output: JFile
+
+	/**
+	 * JPEG compression quality.
+	 */
+	def quality: Float
+
+	/**
+	 * Strength of the Flash Player deblocking filter.
+	 */
+	def deblock: Float
+}
---
+++ scala/apparat/src/apparat/tools/reducer/ReducerConfigurationFactory.scala
@@ -0,0 +1,40 @@
+/*
+ * This file is part of Apparat.
+ *
+ * Apparat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Apparat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Apparat. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2009 Joa Ebert
+ * http://www.joa-ebert.com/
+ *
+ */
+package apparat.tools.reducer
+
+import apparat.tools.{ApparatConfiguration, ApparatConfigurationFactory}
+import java.io.{File => JFile}
+
+/**
+ * @author Joa Ebert
+ */
+object ReducerConfigurationFactory extends ApparatConfigurationFactory[ReducerConfiguration] {
+	override def fromConfiguration(config: ApparatConfiguration): ReducerConfiguration = {
+		val input = new JFile(config("-i") getOrElse error("Input is required."))
+		val output = config("-o") map { pathname => new JFile(pathname) } getOrElse input
+		val deblock = java.lang.Float parseFloat config("-d").getOrElse("0.0")
+		val quality = java.lang.Float parseFloat config("-q").getOrElse("0.99")
+		
+		assert(input.exists, "Input has to exist.")
+
+		new ReducerConfigurationImpl(input, output, quality, deblock)
+	}
+}
