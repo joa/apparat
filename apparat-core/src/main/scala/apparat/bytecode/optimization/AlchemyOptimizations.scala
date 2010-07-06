518eed6e5e80cd18d7c965745a96bff42e5df4e9
Joa Ebert <devnull@localhost>
2010-07-06 19:27:37 +0200

    updated alchemyoptimizations, fixed some bugs but still problems there. updated ignores

--- apparat-core/src/main/scala/apparat/bytecode/optimization/AlchemyOptimizations.scala
+++ apparat-core/src/main/scala/apparat/bytecode/optimization/AlchemyOptimizations.scala
@@ -40,18 +40,18 @@ object AlchemyOptimizations extends (Bytecode => Boolean) {
 					// of mstate.
 					case CallProperty(AbcQName('_mr32, AbcNamespace(22, Symbol(""))), 1) => result = Pop() :: GetInt() :: result; modified = true
 					case CallProperty(AbcQName('_mru16, AbcNamespace(22, Symbol(""))), 1) => result = Pop() :: GetShort() :: result; modified = true
-					case CallProperty(AbcQName('_mrs16, AbcNamespace(22, Symbol(""))), 1) => result = Pop() :: GetShort() :: result; modified = true
+					case CallProperty(AbcQName('_mrs16, AbcNamespace(22, Symbol(""))), 1) => result = Pop() :: Sign16() :: GetShort() :: result; modified = true
 					case CallProperty(AbcQName('_mru8, AbcNamespace(22, Symbol(""))), 1) => result = Pop() :: GetByte() :: result; modified = true
-					case CallProperty(AbcQName('_mrs8, AbcNamespace(22, Symbol(""))), 1) => result = Pop() :: GetByte() :: result; modified = true
+					case CallProperty(AbcQName('_mrs8, AbcNamespace(22, Symbol(""))), 1) => result = Pop() :: Sign8() :: GetByte() :: result; modified = true
 					case CallProperty(AbcQName('_mrf, AbcNamespace(22, Symbol(""))), 1) => result = Pop() :: GetFloat() :: result; modified = true
 					case CallProperty(AbcQName('_mrd, AbcNamespace(22, Symbol(""))), 1) => result = Pop() :: GetDouble() :: result; modified = true
 					// ASC inserts an extra Pop() after CallProperty() since those methods
 					// are typed void. Because of this we do not have to add an extra Pop()
-					case CallProperty(AbcQName('_mw32, AbcNamespace(22, Symbol(""))), 2) => result = SetInt() :: result; modified = true
-					case CallProperty(AbcQName('_mw16, AbcNamespace(22, Symbol(""))), 2) => result = SetShort() :: result; modified = true
-					case CallProperty(AbcQName('_mw8, AbcNamespace(22, Symbol(""))), 2) => result = SetByte() :: result; modified = true
-					case CallProperty(AbcQName('_mwd, AbcNamespace(22, Symbol(""))), 2) => result = SetDouble() :: result; modified = true
-					case CallProperty(AbcQName('_mwf, AbcNamespace(22, Symbol(""))), 2) => result = SetFloat() :: result; modified = true
+					case CallProperty(AbcQName('_mw32, AbcNamespace(22, Symbol(""))), 2) => result = SetInt() :: Swap() :: result; modified = true
+					case CallProperty(AbcQName('_mw16, AbcNamespace(22, Symbol(""))), 2) => result = SetShort() :: Swap() :: result; modified = true
+					case CallProperty(AbcQName('_mw8, AbcNamespace(22, Symbol(""))), 2) => result = SetByte() :: Swap() :: result; modified = true
+					case CallProperty(AbcQName('_mwd, AbcNamespace(22, Symbol(""))), 2) => result = SetDouble() :: Swap() :: result; modified = true
+					case CallProperty(AbcQName('_mwf, AbcNamespace(22, Symbol(""))), 2) => result = SetFloat() :: Swap() :: result; modified = true
 					case o => result = o :: result
 				}
 			} else {
