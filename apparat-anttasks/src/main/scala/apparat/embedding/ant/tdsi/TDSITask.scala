d24784e31e2acf197fe28cd204d1e558a7f4c5d9
Joa Ebert <devnull@localhost>
2012-01-22 19:02:22 +0100

    anttasks, archetypes and asm moved to LGPLv2.

--- apparat-anttasks/src/main/scala/apparat/embedding/ant/tdsi/TDSITask.scala
+++ apparat-anttasks/src/main/scala/apparat/embedding/ant/tdsi/TDSITask.scala
@@ -1,35 +1,31 @@
 /*
  * This file is part of Apparat.
- * 
- * Apparat is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * Apparat is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with Apparat. If not, see <http://www.gnu.org/licenses/>.
- * 
- * Copyright (C) 2009 Joa Ebert
+ *
+ * Copyright (C) 2010 Joa Ebert
  * http://www.joa-ebert.com/
- * 
- * User: Patrick Le Clec'h
- * Date: 8 mai. 2010
- * Time: 17:40:35
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
-package apparat.embedding.ant.tdsi
+ package apparat.embedding.ant.tdsi
 
 import apparat.embedding.ant.{OutParameter, ApparatTask}
 import apparat.tools.tdsi.TurboDieselSportInjection
 
 sealed class TDSITask extends ApparatTask(TurboDieselSportInjection, "tdsi") with OutParameter {
 	def setAlchemy(value: String) = setArgument("a", value)
-	
+
 	def setAlchemyFix(value: String) = setArgument("f", value)
 
 	def setInline(value: String) = setArgument("e", value)
--- apparat-asm/src/main/as3/apparat/asm/GetLocal3.as
+++ apparat-asm/src/main/as3/apparat/asm/GetLocal3.as
@@ -1,23 +1,22 @@
 /*
  * This file is part of Apparat.
  *
- * Apparat is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Apparat is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Apparat. If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright (C) 2009 Joa Ebert
+ * Copyright (C) 2010 Joa Ebert
  * http://www.joa-ebert.com/
  *
- * Author : Patrick Le Clec'h
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 package apparat.asm {
